46fa53f04055b2a983079011ac4a204eeb8f55ec
youna <dbskrltjq@gmail.com>
2022-09-07 12:49:54 +0900

    first commit

---
+++ src/test/java/kr/co/millie/SpringMillieApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.millie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMillieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
